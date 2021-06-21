07fd3d52c32c9bd433aae6a4a75a049c8326f272
Rinat <sv-rinat@mail.ru>
2021-06-21 11:34:04 +0900

    utils

---
+++ pytorch_trainer/utils/utils.py
@@ -0,0 +1,62 @@
+import time
+class LogProgress:
+    """
+    Sort of like tqdm but using log lines and not as real time.
+    """
+
+    def __init__(self, logger, iterable, updates=5, total=None,
+                 name="LogProgress", level=logging.INFO):
+        self.iterable = iterable
+        self.total = total or len(iterable)
+        self.updates = updates
+        self.name = name
+        self.logger = logger
+        self.level = level
+
+    def update(self, **infos):
+        self._infos = infos
+
+    def __iter__(self):
+        self._iterator = iter(self.iterable)
+        self._index = -1
+        self._infos = {}
+        self._begin = time.time()
+        return self
+
+    def __next__(self):
+        self._index += 1
+        try:
+            value = next(self._iterator)
+        except StopIteration:
+            raise
+        else:
+            return value
+        finally:
+            log_every = max(1, self.total // self.updates)
+            # logging is delayed by 1 it, in order to have the metrics from update
+            if self._index >= 1 and self._index % log_every == 0:
+                self._log()
+
+    def _log(self):
+        self._speed = (1 + self._index) / (time.time() - self._begin)
+        infos = " | ".join(f"{k.capitalize()} {v}" for k,
+                           v in self._infos.items())
+        if self._speed < 1e-4:
+            speed = "oo sec/it"
+        elif self._speed < 0.1:
+            speed = f"{1/self._speed:.1f} sec/it"
+        else:
+            speed = f"{self._speed:.1f} it/sec"
+        out = f"{self.name} | {self._index}/{self.total} | {speed}"
+        if infos:
+            out += " | " + infos
+        self.logger.log(self.level, out)
+
+def colorize(text, color):
+    code = f"\033[{color}m"
+    restore = f"\033[0m"
+    return "".join([code, text, restore])
+
+
+def bold(text):
+    return colorize(text, "1")
